14e4099a35003c84b427977a9182383f0729cb16
smsahu12345 <smsahu12345@gmail.com>
2016-11-02 22:55:20 +0530

    Update test-without-vpn-hotstarlivestreamer.bat

--- test-without-vpn-hotstarlivestreamer.bat
+++ test-without-vpn-hotstarlivestreamer.bat
@@ -1,19 +1,23 @@
-@ echo off
+@echo off
 call set /p link=paste the link:
 call set last=%%link:~-1,1%% 
-call set folder="%~dp0\videos\\"
+rem call set folder="%~dp0\videos\\"
+set folder=M:\\LiveStreamer
+If Not Exist %folder% MD %folder%
 call set livestreamer="%~dp0\tools\livestreamer\\"
-call "%~dp0\tools\php5.4\php.exe" test-without-vpn-hotstarlivestreamer.php "%%link%%"
+call "%~dp0\tools\php5.4\php.exe" hotstarlivestreamer.php "%%link%%"
 IF  %last% NEQ c GOTO:nocollection
 call set /p id=enter the Id of the video (example write 1000021386):
-call "%~dp0\tools\php5.4\php.exe" test-without-vpn-hotstarlivestreamer.php "%%link%%" "%%id%%"
+call "%~dp0\tools\php5.4\php.exe" hotstarlivestreamer.php "%%link%%" "%%id%%"
 call set /p quality=write quality (example write 720p):
-call "%~dp0\tools\php5.4\php.exe" test-without-vpn-hotstarlivestreamer.php "%%link%%" "%%id%%" "%%quality%%" "%%folder%%" "%%livestreamer%%"
+call "%~dp0\tools\php5.4\php.exe" hotstarlivestreamer.php "%%link%%" "%%id%%" "%%quality%%" "%%folder%%" "%%livestreamer%%"
 GOTO end1
 :nocollection
-call set /p quality=write quality (example write 720p):
-call set /p choice=play or download? (write p or d):
-call "%~dp0\tools\php5.4\php.exe" test-without-vpn-hotstarlivestreamer.php "%%link%%" "%%quality%%" "%%folder%%" "%%livestreamer%%" "%%choice%%"
+rem call set /p quality=write quality (example write 720p):
+Set quality=720p
+rem call set /p choice=play or download? (write p or d):
+set choice=d
+call "%~dp0\tools\php5.4\php.exe" hotstarlivestreamer.php "%%link%%" "%%quality%%" "%%folder%%" "%%livestreamer%%" "%%choice%%"
 :end1
 pause
 :end
